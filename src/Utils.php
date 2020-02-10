e315f68ad6f94b65778807d943376a8e0186fa92
Romário Beckman <romabeckman@gmail.com>
2020-02-10 08:55:02 -0300

    Add class Utils

---
+++ src/Utils.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Encryption;
+
+class Utils
+{
+
+    static function base64UrlEncode(string $data)
+    {
+        $data = base64_encode($data);
+
+        if (empty($data)) {
+            throw new InvalidArgumentException('$data cant be converted to base64');
+        }
+
+        return rtrim(strtr($data, '+/', '-_'), '=');
+    }
+
+    static function base64UrlDecode(string $data)
+    {
+        $data = base64_decode(strtr($data, '-_', '+/'), true);
+
+        if (empty($data)) {
+            throw new InvalidArgumentException('$data cant be converted to base64');
+        }
+
+        return $data;
+    }
+
+}
